04a43bc2a19e48c01905fdfb33160069883816da
bigduk88 <hangdeuk@naver.com>
2021-06-15 19:29:01 +0900

    first commit

---
+++ src/main/java/com/doitalgo/DoitAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.doitalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitAlgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoitAlgoApplication.class, args);
+    }
+
+}
