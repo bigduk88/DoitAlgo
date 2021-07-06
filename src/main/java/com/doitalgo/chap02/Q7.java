b02138bb93718ad1e05c932ff60f271bc1caeefc
bigduk88 <hangdeuk@naver.com>
2021-07-07 06:40:07 +0900

    [2021.07.07] PrimNumber1

--- src/main/java/com/doitalgo/chap02/Q7.java
+++ src/main/java/com/doitalgo/chap02/Q7.java
@@ -40,23 +40,23 @@ public class Q7 {
         int retry; // 한 번 더?
         char[] cno = new char[32]; // 변환 뒤의 각 자리를 저장하는 문자 배열
 
-        System.out.println("10진수를 기수변환합니다.");
+        System.out.print("10진수를 기수변환합니다.");
         do {
             do {
-                System.out.println("변환하는 음이 아닌 정수:");
+                System.out.print("변환하는 음이 아닌 정수:");
                 no = stdIn.nextInt();
             } while (no < 0);
 
             do {
-                System.out.println("어떤 진수로 변환할까요? (2~36):");
+                System.out.print("어떤 진수로 변환할까요? (2~36):");
                 cd = stdIn.nextInt();
             } while (cd < 2 || cd > 36);
 
             dno = cardConvEx(no, cd, cno); // no를 cd진수로 변환
 
-            System.out.println(cd + "진수로는 ");
+            System.out.print(cd + "진수로는 ");
             for (int i = 0; i < dno; i++)
-                System.out.println(cno[i]);
+                System.out.print(cno[i]);
             System.out.println("입니디ㅏ.");
 
             System.out.println("한번 더 할까요? (1.예/0.아니오):");
