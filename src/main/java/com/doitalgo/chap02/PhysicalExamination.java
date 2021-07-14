8ca994e897601935269ae6651e0d67ecfafe0c61
bigduk88 <hangdeuk@naver.com>
2021-07-15 08:44:44 +0900

    [2021.07.15] Chpt2 Q10

--- src/main/java/com/doitalgo/chap02/PhysicalExamination.java
+++ src/main/java/com/doitalgo/chap02/PhysicalExamination.java
@@ -2,9 +2,9 @@ package com.doitalgo.chap02;
 
 import java.util.Scanner;
 
-public class PysicalExamination {
+public class PhysicalExamination {
 
-    static final int VMMAX = 21; // 시력분포(0.0에서 0.1 단위로 21개)
+    static final int VMAX = 21; // 시력분포(0.0에서 0.1 단위로 21개)
 
     static class PhyscData {
         String name;    // 이름
@@ -33,10 +33,8 @@ public class PysicalExamination {
         int i = 0;
 
         dist[i] = 0;
-
-        dist[i] = 0;
         for (i = 0; i < dat.length; i++)
-            if (dat[i].vision >= 0.0 && dat[i].vision <= VMMAX / 10.0)
+            if (dat[i].vision >= 0.0 && dat[i].vision <= VMAX / 10.0)
                 dist[(int)(dat[i].vision * 10)]++;
     }
 
@@ -52,7 +50,7 @@ public class PysicalExamination {
                 new PhyscData("김영준", 174, 1.2),
                 new PhyscData("박용규", 169, 0.8),
         };
-        int[] vdist = new int[VMMAX];
+        int[] vdist = new int[VMAX];
 
         System.out.println("◼︎ 신체검사 리스트 ◼︎");
         System.out.println("이름       키  시력︎");
@@ -66,7 +64,7 @@ public class PysicalExamination {
         disVision(x, vdist);            // 시력 분포를 구함
 
         System.out.println("\n시력 분포");
-        for (int i = 0; i < VMMAX; i++)
+        for (int i = 0; i < VMAX; i++)
             System.out.printf("%3.1f~ : %2d명\n", i / 10.0, vdist[i]);
     }
 }
